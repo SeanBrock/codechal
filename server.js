75ceade22a45a46d55e8d3f87c0b9ee2fc53bf59
SeanBrock <biker1070@gmail.com>
2017-06-19 16:48:45 -0500

    package.json cleaned up

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res){
-  console.log('getting!')
-  res.send('this message appears in browser?!')
-})
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/', function (req, res){
+//   console.log('getting!')
+//   res.send('this message appears in browser?!')
+// })
 
 app.listen(3000);
 console.log('listening on 3000')
